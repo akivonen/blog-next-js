03331190c1943ec5d8a8e808c8932b95a0b35e9b
Artjom Kivonen <kivonen@gmail.com>
2025-01-15 13:12:31 +0200

    Add images optimization, add metadata

--- components/posts.js
+++ components/posts.js
@@ -1,29 +1,46 @@
-'use client';
+"use client";
 
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
-import { togglePostLikeStatus } from '@/actions/posts';
-import { useOptimistic } from 'react';
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import Image from "next/image";
+import { togglePostLikeStatus } from "@/actions/posts";
+import { useOptimistic } from "react";
+
+// function imageLoader(config) {
+//   const urlStart = config.src.split('upload/')[0];
+//   const urlEnd = config.src.split('upload/')[1];
+//   const transformations = `w_200,q_${config.quality}`;
+//   return `${urlStart}upload/${transformations}/${urlEnd}`;
+// }
 
 function Post({ post, action }) {
-
   return (
     <article className="post">
       <div className="post-image">
-        <img src={post.image} alt={post.title} />
+        <Image
+          // loader={imageLoader}
+          src={post.image}
+          width={128}
+          height={96}
+          alt={post.title}
+          quality={50}
+        />
       </div>
       <div className="post-content">
         <header>
           <div>
             <h2>{post.title}</h2>
             <div>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
             </div>
           </div>
-          <form action={action.bind(null, post.id)} className={post.isLiked ? 'liked' : ''}>
+          <form
+            action={action.bind(null, post.id)}
+            className={post.isLiked ? "liked" : ""}
+          >
             <LikeButton />
           </form>
         </header>
@@ -34,21 +51,26 @@ function Post({ post, action }) {
 }
 
 export default function Posts({ posts }) {
-  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(posts, (prevPosts, updatedPostId) => {
-    const updatedPostIndex = prevPosts.findIndex((post) => post.id === updatedPostId);
+  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(
+    posts,
+    (prevPosts, updatedPostId) => {
+      const updatedPostIndex = prevPosts.findIndex(
+        (post) => post.id === updatedPostId
+      );
 
-    if(updatedPostIndex === -1) {
-      return prevPosts;
+      if (updatedPostIndex === -1) {
+        return prevPosts;
+      }
+
+      const updatedPost = { ...prevPosts[updatedPostIndex] };
+      updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+      updatedPost.isLiked = !updatedPost.isLiked;
+      const newPosts = [...prevPosts];
+      newPosts[updatedPostIndex] = updatedPost;
+
+      return newPosts;
     }
-
-    const updatedPost = { ...prevPosts[updatedPostIndex] };
-    updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
-    updatedPost.isLiked = !updatedPost.isLiked;
-    const newPosts = [...prevPosts];
-    newPosts[updatedPostIndex] = updatedPost;
-
-    return newPosts;
-  });
+  );
 
   async function updatePost(postId) {
     updateOptimisticPosts(postId);
